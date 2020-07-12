1b19a8ecaec681dfbac5fc05dd94b0acced2fd7f
gekomod <zaba141@o2.pl>
2020-07-12 07:39:03 +0200

    Update CommentType.php

--- src/Mert/TicketBundle/Form/CommentType.php
+++ src/Mert/TicketBundle/Form/CommentType.php
@@ -5,13 +5,15 @@ namespace Mert\TicketBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CommentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('ticketId', 'entity' , array('class' => 'Mert\TicketBundle\Entity\Ticket'));
-        $builder->add('comment', 'textarea', array('label' => false));
+        $builder->add('ticketId', EntityType::class, array('class' => 'Mert\TicketBundle\Entity\Ticket'));
+        $builder->add('comment', TextareaType::class, array('label' => false));
     }
 
     public function configureOptions(OptionsResolver $resolver)
